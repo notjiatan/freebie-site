2f7394e91bbc68ef6d6e75671e88245c6a60d68b
Anon <224639307+JiaTan-0@users.noreply.github.com>
2025-09-23 04:54:24 +0100

    updated fonts (not working) & added glitching glowing homepage text freebie finders

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,9 @@ let h3 = h4 * font_scale;
 let h2 = h3 * font_scale;
 let h1 = h2 * font_scale;
 
-let fontPrimary = "serif";
-let fontPrimaryType = "serif";
-let fontSecondary = "open sans";
+let fontPrimary = "Ubuntu, sans-serif";
+let fontPrimaryType = "sans-serif";
+let fontSecondary = "Ubuntu, sans-serif";
 let fontSecondaryType = "sans-serif";
 
 fontPrimary = fontPrimary
